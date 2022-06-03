9d854e14dc3247f6fd068ea45ee0794f4fa8318d
husnain <husnainmahooon@gmail.com>
2022-06-03 16:32:44 +0500

    Remove junk

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
-import { BrowserRouter,Routes,Route,Link} from 'react-router-dom';
+import { BrowserRouter,Routes,Route,Link,useHistory,Redirect} from 'react-router-dom';
 import './App.css';
 import Login from './login';
 import Forget from './forget';
 import Registration from './registration';
 import Home from './home';
+import App from './usestate';
+import Wellcome from './Wellcomepage';
+
 
 
 
@@ -16,10 +19,12 @@ export default function () {
   <BrowserRouter>
   
     <Routes>
+    <Route path='/usestate'element={<App></App>}></Route>
     <Route path="/" element={<Home></Home>}></Route>
     <Route path="/Login" element={<Login></Login>}></Route>
     <Route path="/Registration" element={<Registration></Registration>}></Route>
     <Route path="/Forget" element={<Forget></Forget>}></Route>
+    <Route path="/Wellcomepage" element={<Wellcome></Wellcome>}></Route>
 
     </Routes>
   </BrowserRouter>
--- src/registration.js
+++ src/registration.js
@@ -1,20 +1,75 @@
-import { Link } from "react-router-dom";
+
+import { useState } from "react";
+import {Link,useNavigate} from "react-router-dom";
 export default function Registration(){
+  const [firstName,setFirstName]=useState("husnain");
+  const [surName,setSurename]=useState();
+  const [Password,setPassword]=useState();
+  const [Email,SetEmail]=useState();
+  // const [submit,SetSubmit]=useState(false);
+
+  const onEmail = (e)=>{
+    console.log(e.target.name);
+    if (e.target.name==="isEmail"){
+      SetEmail(e.target.value);
+      
+    }
+  }
+  const onPassWord = (e)=>{
+    console.log(e.target.name);
+    if(e.target.name==="PassWord"){
+      setPassword(e.target.value);
+    }
+    
+  }
+  const onSureName = (e)=>{
+    console.log(e.target.name);
+    if (e.target.name==="Sname"){
+      setSurename(e.target.value);
+    }
+  }
+  const onTextFieldChang = (e)=>{
+    console.log(e.target.name);
+    if (e.target.name==="fName"){
+      setFirstName(e.target.value);
+    }
+  }
+  const navigate = useNavigate();
+  const handlRegister =  (e)=>{
+   
+    console.log(firstName);
+    console.log(surName);
+    console.log(Email);
+    console.log(Password);
+    if (firstName && surName && Email && Password){
+      // SetSubmit(true);
+      navigate("/Wellcomepage");}
+    else {
+        alert("plz complete all inputs")
+      }
+    
+  }
+  
+ 
+   
+  
     return <div className="block"> 
     <form>
             <h1>Registration Form </h1>
-            <label>First Name </label><br />
-            <input type="text" placeholder="Husnain" /><br />
-            <label>Second Name </label><br />
-            <input type="text" placeholder="Mahoon" /> <br />
+            <label>firstName </label><br />
+            <input name="fName" type="text"  onChange={onTextFieldChang} value={firstName} placeholder="Husnain" /><br />
+            <label>Surname </label><br />
+            <input name="Sname" type="text" onChange={onSureName} value={surName}  placeholder="Mahoon" /> <br />
             <label>Email</label> <br />
-            <input type="text" placeholder="Email" /> <br />
+            <input type="email" name="isEmail" onChange={onEmail} value={Email} placeholder="Email" /> <br />
+        
             <label>Password</label><br />
-            <input type="password" placeholder="*******" />
+            <input name="PassWord" type="password" onChange={onPassWord} value={Password} placeholder="*******" />
+            <input type="button" onClick={handlRegister} name="submit" value="Submit" />
           </form>
-          <button>Submit</button> 
+          
           <div> <Link to="/Login">I am already a member</Link></div>
           
      
     </div>
-}
+    }
